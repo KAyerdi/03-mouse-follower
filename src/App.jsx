baaf4dbc9fee088024e6966a599d47dea67b9403
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2023-08-29 16:45:05 -0300

    actualizando martes 29

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
-  const [position, setPosition] = useState ({x: 0, y: 0})
+  const [position, setPosition] = useState ({ x: 0, y: 0 })
   
   useEffect(() => {
     console.log ('efecto', { enabled })
@@ -17,26 +17,44 @@ function App() {
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
     }
+    //cleanup
+    // --> se ejecuta cuando el componente se desmonta,
+    // --> cuando cambian las dependencias, antes de ejecutar
+    // --> el efecto de nuevo
+    return () => {
+      console.log ("cleanup")
+      window.removeEventListener('pointermove', handleMove)
+    }
   }, [enabled])
 
   return (
+    <>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: '#09f',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+      transform: `translate(${position.x}px, ${position.y}px)`
+    }}
+    />
+    <button onClick={() => setEnabled(!enabled)}>
+    {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+    </button>
+    </>
+)
+}
+
+function App() {
+  const [mounted, setMounted] = useState(true)
+
+  return (
     <main>
-      <div style={{
-        position:'absolute',
-        backgroundColor: '#09f',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
-        transform: 'translate(${position.x}px, ${position.y}px)',
-      }} />
-      <h3>Proyecto 3</h3>
-      <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
-        </button>
+    <FollowMouse/>
     </main>
   )
 }
